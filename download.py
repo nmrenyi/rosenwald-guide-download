8608520e9191909c965d5594b73033dd1a8f7eff
nmrenyi <nmrenyi@outlook.com>
2025-07-30 16:21:09 +0200

    feat: finish basic structure

--- download.py
+++ download.py
@@ -1,5 +1,20 @@
+
+def get_catalogue(url):
+    pass
+
+def parse_catalogue(file):
+    pass
+
+def download_files(file_urls):
+    pass
+
+
+
 def main():
-    print("Hello, World!")
+    catalogue_url = 'https://gallica.bnf.fr/ark:/12148/cb344120051/date'
+    catelogue_file = get_catalogue(catalogue_url)
+    file_urls = parse_catalogue(catelogue_file)
+    download_files(file_urls)
 
 if __name__ == "__main__":
     main()
